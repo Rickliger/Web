db85c50e9da933df3a0873e71384b402750f204d
Rickliger <104471511+Rickliger@users.noreply.github.com>
2022-08-06 14:26:39 -0300

    Trabalhando com Condições
    Desenvolvendo Funções em JS

---
+++ JavaScript/aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 78.2
+console.log(`A velocidade do seu carro é ${vel}KM/h`)
+if (vel > 60){
+    console.log('Voce ultrpassou a velocidade. MULTADO!')
+}
+console.log('Dirija sempre usando o sinto de segurança')
---
+++ JavaScript/aula11/ex009.js
@@ -0,0 +1,8 @@
+var pais = 'FRA'
+console.log(`Vivendo em ${pais}`)
+if (pais != 'BR'){
+    console.log('BRASILEIRO!')
+}
+else{
+    console.log('ESTRANGEIRO')
+}
